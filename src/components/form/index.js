300b17eb08c87a03d200f4086b7feeb95af2fc51
Sara Al-tayeh <saraaltayeh1@gmail.com>
2022-08-21 02:58:15 +0300

    update history

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,16 +1,81 @@
-import React from 'react';
-import  { useState } from "react";
+// import React from 'react';
+// import  { useState } from "react";
+// import Badge from 'react-bootstrap/Badge';
+// import './form.scss';
+
+// function Form(props) {
+//   const [method,setMethod]=useState('GET')
+//   const [url,setUrl]=useState('https://pokeapi.co/api/v2/pokemon')
+//   const [request, setRequest] = useState('');
+//   const [TextArea,setTextArea] = useState(false);
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     const formData = {
+//       method:method,
+//       url: url,
+//       request:request
+//     };
+//     props.handleApiCall(formData);
+//   }
+
+// const methodFunction = (e)=> {
+//   setMethod(e.target.id)
+//   setTextArea(false);
+// }
+// const urlFunction = (e)=> {
+//   setUrl(e.target.value)
+// }
+// const requestFunction = (e)=> {
+//   setRequest(e.target.value)
+// }
+
+// function handleTextArea(e){
+//   e.preventDefault()
+//   setTextArea(true);
+//   setMethod(e.target.id);  
+// }
+
+//     return (
+//       <>
+//         <form onSubmit={handleSubmit}>
+//           <label >
+//             <span>URL: </span>
+//             <input name='url'data-testid='button' onChange={urlFunction}  type='text' />
+//             <button variant="success" type="submit">GO!</button>{' '}
+//           </label>
+//           <label className="methods">
+//           <Badge bg="primary" id="get" onClick={methodFunction}>GET</Badge>
+//             <span id="post" onClick={handleTextArea}>POST</span>
+//             <span id="put" onClick={handleTextArea}>PUT</span>
+//             <span id="delete" onClick={methodFunction}>DELETE</span>
+//           </label>
+//           <div>
+//         <label>Enter value : </label>
+//         {
+//           TextArea &&
+//         <textarea onChange={requestFunction} rows={5} cols={35}/>
+//         }
+//       </div>
+//         </form>
+//       </>
+//     );
+//     }
+
+// export default Form;
+
+
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge';
 import './form.scss';
 
-function Form(props) {
-  const [method,setMethod]=useState('GET')
-  const [url,setUrl]=useState('https://pokeapi.co/api/v2/pokemon')
-  const [request, setRequest] = useState('');
-  const [TextArea,setTextArea] = useState(false);
-
-  function handleSubmit(e) {
+function Form (props) {
+const [method,setMethod]=useState('GET')
+const [url,setUrl]=useState('https://pokeapi.co/api/v2/pokemon')
+let [TextArea,setTextArea] = useState(false);
+const [request, setRequest] = useState('');
+  function handleSubmit (e) {
     e.preventDefault();
     const formData = {
       method:method,
@@ -19,16 +84,12 @@ function Form(props) {
     };
     props.handleApiCall(formData);
   }
-
-const methodFunction = (e)=> {
-  setMethod(e.target.id)
+const methodFunction = (x)=> {
+  setMethod(x.target.id)
   setTextArea(false);
 }
-const urlFunction = (e)=> {
-  setUrl(e.target.value)
-}
-const requestFunction = (e)=> {
-  setRequest(e.target.value)
+const urlFunction = (x)=> {
+  setUrl(x.target.value)
 }
 
 function handleTextArea(e){
@@ -36,31 +97,40 @@ function handleTextArea(e){
   setTextArea(true);
   setMethod(e.target.id);  
 }
-
+const requestSetState = (e) => {
+  setRequest(e.target.value);
+}
     return (
       <>
         <form onSubmit={handleSubmit}>
           <label >
             <span>URL: </span>
-            <input name='url' data-testid='button' onChange={urlFunction}  type='text' />
-            <button variant="success" type="submit">GO!</button>
+            <input name='url'data-testid='button' onChange={urlFunction}  type='text' />
+            <button variant="success" type="submit">GO!</button>{' '}
           </label>
           <label className="methods">
-          <Badge bg="primary" id="get" onClick={methodFunction}>GET</Badge>
-            <span id="post" onClick={handleTextArea}>POST</span>
-            <span id="put" onClick={handleTextArea}>PUT</span>
-            <span id="delete" onClick={methodFunction}>DELETE</span>
+            <span bg="primary" id="get" onClick={methodFunction}>GET</span>
+            <span id="post"onClick={handleTextArea}>POST</span>
+            <span id="put"onClick={handleTextArea}>PUT</span>
+            <span id="delete"onClick={methodFunction}>DELETE</span>
           </label>
           <div>
-        <label>Enter value : </label>
+          <label>Enter value : </label> 
         {
-          TextArea &&
-        <textarea onChange={requestFunction} rows={5} cols={35}/>
-        }
+          
+         TextArea &&
+        
+        <textarea
+           
+          onChange={requestSetState}
+          rows={8}
+          cols={35}
+        />}
       </div>
         </form>
       </>
     );
-    }
+  
+}
 
 export default Form;
--- src/reducer.js
+++ src/reducer.js
@@ -1,30 +1,23 @@
-const first = {
-    url: "",
-    method: "",
-    result: []
-}
+const first = []
 export default function historyReducer(state = first, action) {
     const {
         type,
         payload
     } = action;
-    // console.log("rrrrrrrrrrrr",payload.data);
+
     switch (type) {
         case "ADD":
             const url = payload.divData.url;
             const method = payload.divData.method;
+            
             return ([...state, [{
                 url: url,
                 method: method,
                 result: payload.data
             }]]);
     }
-
 }
 export function add(last) {
-    // console.log("add");
-    // console.log("div ",divData);
-    // console.log("resulit  ",data);
     return {
         type: "ADD",
         payload: last,
